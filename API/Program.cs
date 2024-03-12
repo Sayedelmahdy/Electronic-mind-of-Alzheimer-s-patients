6cab31f79b6318e462e2d22a2fbd13238a97050b
Sayedelmahdy <sayedahmed01093307397@gmail.com>
2024-03-12 05:22:54 +0200

    Done Auth

--- API/Program.cs
+++ API/Program.cs
@@ -125,11 +125,12 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+}*/
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
